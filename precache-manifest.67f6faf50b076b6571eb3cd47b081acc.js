504a7468aa8f475274903c0b58f3f713ffcdc5a8
hitzu <hitzu1001@gmail.com>
2020-01-23 17:25:51 +1100

    Updates

---
+++ precache-manifest.67f6faf50b076b6571eb3cd47b081acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c542f3caf0c89db5722017324ac4359",
+    "url": "/React_TodoList_Project/index.html"
+  },
+  {
+    "revision": "a98382dde7724dfec7b0",
+    "url": "/React_TodoList_Project/static/css/main.514fbe82.chunk.css"
+  },
+  {
+    "revision": "9bc9d53b7e93e48f3e03",
+    "url": "/React_TodoList_Project/static/js/2.10413e1b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React_TodoList_Project/static/js/2.10413e1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a98382dde7724dfec7b0",
+    "url": "/React_TodoList_Project/static/js/main.5c10c3bd.chunk.js"
+  },
+  {
+    "revision": "ecc8b01fb6876b1084eb",
+    "url": "/React_TodoList_Project/static/js/runtime-main.75fde15a.js"
+  }
+]);
